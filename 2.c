6c3e2450810990fcee75b2db04ae257213528090
historypiat-cyber <historypiat@gmail.com>
2026-03-29 15:29:19 +0000

    Update 2.c

--- 2.c
+++ 2.c
@@ -8,47 +8,59 @@
 
 #include <stdio.h>
 
-   struct info{
-        char PIB[15];
-        int matan;
-        int programming;
-        int algebra;
-    };
-    
-    
-int main()
-{
-   int n;
-    printf("Введіть кількість студентів:");
-    scanf("%d",&n);
-    
-    struct info data[n];
-    int grade[n];
-    int count;
-    
- 
-   for(int i = 0; i<n; i++){
-       printf("Прізвище ініціали: ");
-       scanf("%c",&data[i].PIB);
-       printf("Матаналіз: ");
-       scanf("%d",&data[i].matan);
-       printf("Прізвище ініціали: ");
-       scanf("%d",&data[i].programming);
-       printf("Прізвище ініціали: ");
-       scanf("%d",&data[i].algebra);
-       
-       grade[i] = (  data[i].algebra + data[i].matan + data[i].programming )/3;
-   }
-   for(int i = 0; i<n; i++){
-       printf("студент %c порівняння з іншими:",data[i].PIB);
-       for(int j = i+1; j<n; j++){
-           int result = (  (grade[i] - grade[j]) / grade[j]  ) * 100;
-           printf("%c",)
-       }
-   }
-   
-   
+struct info {
+    char PIB[50];
+    int matan;
+    int programming;
+    int algebra;
+};
 
-   
+int main() {
+    int n;
+    printf("Введіть кількість студентів:");
+    scanf("%d", &n);
+
+    struct info data[n];
+    float grades[n];
+
+    for (int i=0; i<n; i++) {
+        printf("\nСтудент %d:\n",i + 1);
+        printf("Прізвище ініціали:");
+        scanf(" %s",data[i].PIB);
+        
+        printf("Матаналіз:");
+        scanf("%d",&data[i].matan);
+        
+        printf("Програмування:");
+        scanf("%d",&data[i].programming);
+        
+        printf("Алгебра:");
+        scanf("%d",&data[i].algebra);
+
+        grades[i] = (  (data[i].matan + data[i].programming + data[i].algebra) / 3.0  ); //середній бал
+    }
+
+    printf("студенти з оцінками 4 і 5");
+
+    for (int i=0; i<n; i++) {
+        if (grades[i] >= 4) {
+            printf("Студент: %s (Середній бал: %.2f)\n", data[i].PIB, grades[i]);
+            
+            for (int j = 0; j<n;j++) {
+                if (i == j) continue;
+                
+                float diff = (  (grades[i] - grades[j]) / grades[j]  ) * 100;
+                
+                if (diff>0) {
+                    printf("Краще за %s на %.1f%%\n", data[j].PIB, diff);
+                } else if (diff<0) {
+                    printf("Гірше за %s на %.1f%%\n", data[j].PIB, -diff);
+                } else {
+                    printf("Бали однакові з %s\n", data[j].PIB);
+                }
+            }
+        }
+    }
+
     return 0;
 }
